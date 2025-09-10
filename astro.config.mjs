643df1e849fa5e1d5f6cb7b3b7cb9fd409e7152e
futzone <qahhorovzamon@gmail.com>
2025-09-10 13:46:52 +0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,100 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+
+	integrations: [
+		starlight({
+			favicon: '/favicon.svg',
+			title: 'Biznex Docs',
+			customCss: [
+				'./src/styles/custom.css',
+			],
+			sidebar: [
+				{
+					label: 'Boshlash',
+					items: [
+						{ label: '🚪 Kirish', slug: 'getting-started/introduction' },
+						{ label: '💻 O\'rnatish', slug: 'getting-started/installation' },
+						{ label: '⚡️ Tezkor boshlash', slug: 'getting-started/quick-start' },
+					],
+				},
+
+				{
+					label: 'Asosiy tushunchalar',
+					items: [
+						{ label: '👩‍🍳 Xodimlar', slug: 'core-modul/employees' },
+						{ label: '🥕 Mahsulotlar', slug: 'core-modul/ingredients' },
+						{ label: '🍛 Taomlar', slug: 'core-modul/products' },
+						{ label: '📄 Retseptlar', slug: 'core-modul/recipes' },
+						{ label: '🛒 Xaridlar', slug: 'core-modul/shopping' },
+						{ label: '💳 Tranzaksiyalar', slug: 'core-modul/transactions' },
+					],
+				},
+
+				{
+					label: 'POS tizimi',
+					items: [
+						{ label: '🪑 Joylar & o\'rinlar', slug: 'pos-modul/places' },
+						{ label: '〽️ Savdo qilish', slug: 'pos-modul/sales' },
+						{ label: '🛎 Buyurtmalar', slug: 'pos-modul/orders' },
+						{ label: '👥 Mijozlar', slug: 'pos-modul/customers' },
+
+					],
+				},
+
+				{
+					label: 'Hisobotlar',
+					items: [
+						{ label: '📊 Umumiy ko\'rinish', slug: 'inventory-modul/overview' },
+						{ label: '📉 Asosiy hisobotlar', slug: 'inventory-modul/monitoring' },
+						{ label: '🍱 Taomlar', slug: 'inventory-modul/stock-management' },
+						{ label: '🏢 Omborxona boshqaruvi', slug: 'inventory-modul/warehouse-management' },
+
+					],
+				},
+				{
+					label: 'Qurilmalar',
+					items: [
+						{ label: '🖥 POS qurilma', slug: 'devices-modul/desktop' },
+						{ label: '📱 Mobil ilova', slug: 'devices-modul/mobile' },
+						{ label: '🌐 Ulanish', slug: 'devices-modul/connection' },
+
+					],
+				},
+				{
+					label: 'Sozlamalar',
+					items: [
+						{ label: '🔑 Boshqaruv', slug: 'config-modul/roles' },
+						{ label: '📲 Integratsiya', slug: 'config-modul/integrations' },
+						{ label: '⚙️ Ilova sozlamalari', slug: 'config-modul/settings' }
+					],
+				},
+
+				{
+					label: 'FAQ',
+					items: [
+						{ label: '❓ FAQ 1', slug: 'faq-modul/faq-1' },
+						{ label: '❓ FAQ 2', slug: 'faq-modul/faq-2' },
+					],
+				},
+
+				{
+					label: 'Boshqalar',
+					items: [
+						{ label: '🛠 Versiyalar', slug: 'support-modul/changelog' },
+						{ label: '💲 Obuna sotib olish', slug: 'support-modul/demo' },
+						{ label: '📞 Qo\'llab-quvvatlash', slug: 'support-modul/support' },
+						
+					 
+					],
+				},
+
+				 
+
+			],
+		}),
+	],
+});
---
+++ src/content/docs/getting-started/introduction.md
@@ -0,0 +1,48 @@
+---
+title: Biznex POS qo'llanmaga kirish
+description: Restoran, kafe va choyxonalar uchun zamonaviy savdo va ombor boshqaruv tizimi.  
+---
+
+**Biznex POS** — bu restoran, kafe va choyxonalar uchun zamonaviy savdo va ombor boshqaruv tizimi.  
+Tizim sizga nafaqat savdoni, balki **masalliqlarni, retseptlarni, xarajatlarni va moliyaviy oqimlarni** ham to‘liq nazorat qilish imkoniyatini beradi.  
+
+---
+
+## 🚀 Nega Biznex POS?
+
+- 🔹 **Tez va qulay** — oddiy interfeys va tezkor ishlash.  
+- 🔹 **Masalliqlarni nazorat qilish** — retsept asosida avtomatik hisob-kitob.  
+- 🔹 **Ombor boshqaruvi** — FIFO/FEFO standartlari bilan.  
+- 🔹 **Moliyaviy hisobotlar** — daromad/xarajatni real vaqtda kuzatish.  
+- 🔹 **Ko‘p foydalanuvchilik** — kassirlar, menejerlar va adminlar uchun rollar.  
+- 🔹 **Cloud va Offline** — internet bo‘lmasa ham ishlaydi.  
+
+---
+
+## 📂 Hujjatlar tuzilishi
+
+- **Boshlash** → O‘rnatish va dastlabki sozlamalar.  
+- **Asosiy tushunchalar** → Ingredient, Recipe, Shopping, Transaction.  
+- **POS tizimi** → Savdo, buyurtmalar, mijozlar, stollar.  
+- **Hisobotlar** → Ombor, mahsulotlar, hisobotlar.  
+- **Qurilmalar** → Desktop, Android, iOS.  
+- **Sozlamalar** → Sozlamalar, rollar va integratsiyalar.  
+- **FAQ** → Ko'p so'raladigan savollar.  
+- **Boshqalar** → Versiyalar va qo'llab quvvatlash.  
+
+---
+## ❓ Kimlar uchun?
+
+Biznex Docs — bu **Biznex POS** tizimidan foydalanadigan barcha foydalanuvchilar uchun yaratilgan rasmiy hujjatlar to‘plami bo'lib, undan quyidagi shaxslar foydalanishi mumkin:
+
+- **Restoran va kafe egalari**  
+  Omborxona, menyu va sotuvlarni nazorat qilish uchun.
+
+- **Xodimlar (ofitsiant, kassir, oshpaz)**  
+  Kundalik ish jarayonlarini qulayroq va aniqroq yuritish uchun.
+
+- **Buxgalter va menejerlar**  
+  Xarajatlar, daromadlar va tranzaksiyalarni kuzatib borish uchun.
+
+- **Biznex xodimlari**  
+  Biznex POS’ni o'rnatish bilan shug'ullanadiganlar yoki shug'ullanmoqchi bo'lganlar uchun
